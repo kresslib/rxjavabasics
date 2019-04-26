fcb017daba770d429fe1cc000791b6b85cc154f3
Olesiia7 <olesiia7@bk.ru>
2019-04-26 18:12:45 +0300

    init

---
+++ SuperHeroesService/src/main/java/Code3.java
@@ -0,0 +1,15 @@
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.ext.web.client.HttpResponse;
+
+
+public class Code3 {
+
+    public static void main(String[] args) {
+        SuperHeroesService.run();
+
+        Helpers.client().get("/heroes").rxSend()
+                .map(HttpResponse::bodyAsJsonObject)
+                .map(JsonObject::size)
+                .subscribe(length -> System.out.println("Number of heroes: " + length));
+    }
+}
